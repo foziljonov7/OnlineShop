44614f3036184e98591f8b385f32e74ce910a185
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2024-02-14 12:08:34 +0500

    Create Product validations

---
+++ OnlineShop.Api/Validator/ImaeValidator/CreateImageValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using OnlineShop.Api.Data;
+using OnlineShop.Api.Dtos.ImageDtos;
+
+namespace OnlineShop.Api.Validator.ImaeValidator
+{
+    public class CreateImageValidator : AbstractValidator<CreateImageDto>
+    {
+        public CreateImageValidator(AppDbContext dbContext)
+        {
+            RuleFor(dto => dto.FileName)
+                .NotEmpty().WithMessage("FileName should not be empty");
+
+            RuleFor(dto => dto.FilePath)
+                .NotEmpty().WithMessage("FilePath should not be empty");
+
+            RuleFor(dto => dto.ProductId)
+                .NotNull().WithMessage("ProductId cann't be null");
+        }
+    }
+}
---
+++ OnlineShop.Api/Validator/ProductValidators/CreateProductValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using OnlineShop.Api.Data;
+using OnlineShop.Api.Dtos.ProductDtos;
+
+namespace OnlineShop.Api.Validator.ProductValidators
+{
+    public class CreateProductValidator : AbstractValidator<CreateProductDto>
+    {
+        public CreateProductValidator(AppDbContext dbContext)
+        {
+            RuleFor(dto => dto.Title)
+                .NotEmpty().WithMessage("Name should not be empty")
+                .MinimumLength(2).MaximumLength(50);
+
+            RuleFor(dto => dto.Description)
+                .MaximumLength(256).WithMessage("Description maximal 256 characters");
+
+            RuleFor(dto => dto.Price)
+                .NotNull().WithMessage("Price cann't be null");
+
+            RuleFor(dto => dto.Quantity)
+                .NotNull().WithMessage("Quantity cann't be null");
+
+            RuleFor(dto => dto.CategoryId)
+                .NotNull().WithMessage("CategoryId cann't be null");
+        }
+    }
+}
